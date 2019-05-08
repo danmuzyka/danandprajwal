839f0ea29332047d40df37f524c9f512082edcb7
Dan O'Brien Muzyka <dan@danmuzyka.com>
2019-05-08 00:18:10 -0700

    Setting up deployment tools for AWS S3 and Cloudfront, plus fixing code compatibility issues affecting deployment.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 });
+const url = require('url');
 
-const googleCredentials = require(process.env.GOOGLE_SERVICE_ACCOUNT_CREDENTIALS);
+const targetAddress = url.parse(process.env.TARGET_ADDRESS || `https://www.danandprajwal.com`);
 
 module.exports = {
   siteMetadata: {
@@ -19,43 +20,6 @@ module.exports = {
         name: `src`,
         path: `${__dirname}/src/`,
       },
-    },
-    {
-      resolve: `gatsby-source-google-spreadsheet`,
-      options: {
-        // The `spreadsheetId` is required, it is found in the url of your document:
-        // https://docs.google.com/spreadsheets/d/<spreadsheetId>/edit#gid=0
-        spreadsheetId: `1DQgRxQA4A3AY6j0F2HUrar8XWBNqvqUTFi064qRJ4w0`,
-
-        // The `spreadsheetName` is recommended, but optional
-        // It is used as part of the id's during the node creation, as well as in the generated GraphQL-schema
-        // If you are sourcing multiple sheets, you can set this to distinguish between the source data
-        spreadsheetName: `DanAndPrajwalWebsiteContent`,
-
-        // The `typePrefix` is optional, default value is "GoogleSpreadsheet"
-        // It is used as part of the id's during the node creation, as well as in the generated GraphQL-schema
-        // It can be overridden to fully customize the root query
-        typePrefix: `GoogleSpreadsheet`,
-
-        // The `credentials` are only needed when you need to be authenticated to read the document.
-        // It's an object with the following shape:
-        // {
-        //   client_email: "<your service account email address>",
-        //   private_key: "<the private key for your service account>"
-        // }
-        //
-        // Refer to googles own documentation to retrieve the credentials for your service account:
-        //   - https://github.com/googleapis/google-api-nodejs-client#service-to-service-authentication
-        //   - https://developers.google.com/identity/protocols/OAuth2ServiceAccount
-        //
-        // When you have generated your credentials, it's easiest to refer to them from an environment variable
-        // and parse it directly:
-        credentials: googleCredentials,
-
-        // The `mapValue(value, key, cell)` function lets you map each cell's value to your need.
-        // For example, it can be used to ensure that all strings are trimmed:
-        mapValue: value => typeof value === "string" ? value.trim() : value
-      }
     },
     `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
@@ -77,5 +41,21 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-plugin-s3`,
+      options: {
+        bucketName: 'danandprajwal',
+        region: 'us-west-2',
+        acl: null,
+        protocol: targetAddress.protocol.slice(0, -1),
+        hostname: targetAddress.hostname,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: targetAddress.href.slice(0, -1),
+      },
+    },
   ],
 };
--- src/components/RSVP/index.js
+++ src/components/RSVP/index.js
@@ -3,8 +3,8 @@ import React from "react"
 const RSVP = () => {
   return (
     <iframe id="RSVPifyIFrame" style={{width: '100%', border: 'none', overflow: 'hidden', height: '1175px'}}
-            onLoad={window.parent.scrollTo(0,0)} title="RSVP Form"
-            src="http://danandprajwal.app.rsvpify.com/?embed=1&amp;js=1" frameBorder="0" scrolling="no" />
+            title="RSVP Form"
+            src="https://danandprajwal.app.rsvpify.com/?embed=1&amp;js=1" frameBorder="0" scrolling="no" />
   );
 };
 
