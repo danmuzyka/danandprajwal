d5a68f3801b1e4958f6dc07ee585b1fd707e5d36
Dan O'Brien Muzyka <dan@danmuzyka.com>
2019-05-08 23:15:37 -0700

    Implementing iframe-resizer on RSVPify form.

--- src/components/RSVP/index.js
+++ src/components/RSVP/index.js
@@ -1,10 +1,18 @@
 import React from "react"
+import ReactIframeResizer from "react-iframe-resizer-super"
+
+const iframeResizerOptions = {
+  autoResize: true,
+  checkOrigin: false,
+  heightCalculationMethod: 'max',
+  enablePublicMethods: true
+};
 
 const RSVP = () => {
   return (
-    <iframe id="RSVPifyIFrame" style={{width: '100%', border: 'none', overflow: 'hidden', height: '1175px'}}
-            title="RSVP Form"
-            src="https://danandprajwal.app.rsvpify.com/?embed=1&amp;js=1" frameBorder="0" scrolling="no" />
+    <ReactIframeResizer id="RSVPifyIFrame" iframeResizerOptions={iframeResizerOptions}
+            title="RSVP Form" style={{width: '100%', border: 'none', overflow: 'hidden', height: '1175px'}}
+            src="https://danandprajwal.app.rsvpify.com/?embed=1&js=1" frameBorder={0} scrolling="no" />
   );
 };
 
